f147b7d8ef4c761780b8d089f39dd2d8ff0f1e1c
Barrett LaFrance <lghrants@gmail.com>
2017-10-23 18:29:09 -0500

    feat: install nvm and specify desired version of nodejs

--- .provisioning/dev.sh
+++ .provisioning/dev.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-# install yarn / nodejs
+# install nvm & nodejs
+NVM_VERSION="0.33.4"
+NVM_INSTALL="https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh"
+su - vagrant -c "curl -o- $NVM_INSTALL | bash"
+
+NODE_VERSION="8.7.0"
+su - vagrant -c "\
+    source ~/.bashrc && \
+    nvm install v${NODE_VERSION} && \
+    nvm alias default $NODE_VERSION"
+
+# install yarn
 wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 dnf install yarn -y
 
